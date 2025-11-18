f3356748557c20c409cc511ffeec270baff1140d
White-Town <nareshbc5555@gmail.com>
2025-11-18 09:17:54 +0000

    basics of js

---
+++ javascript/index.js
@@ -0,0 +1,85 @@
+// let x= 40;
+// let y = "40";
+// console.log(x==y);
+// console.log(x===y);
+
+
+// let a = "120ac";
+// let b = Number(a);
+// console.log(b);
+// console.log(typeof(b));
+// // convert number to string
+
+// let c = 34;
+// let z = String(c);
+// console.log(z);
+// console.log(typeof(z));
+
+// console.log(Number(false));
+// console.log(Number(null));
+// console.log(Number(undefined))
+// console.log(Number(true));
+
+// console.log(Boolean(null));
+
+// let x = 0.2;
+// let y = 0.1;
+// let z = x+ y;
+// console.log(z===0.3);
+// console.log(z);
+
+
+// let x = String(0.2);
+// let y = String(0.1);
+
+// let z = x + y;
+
+// console.log(z, typeof(z));
+
+
+// null is loosely equal to undefined only not with any other
+
+// console.log(null == undefined); //loosely equal
+
+// console.log(null === undefined); //tightly comparison false
+// console.log(Number(null));
+// console.log(null);
+// console.log(typeof(null));
+
+// >,<, >=, <= (null --> number)
+
+// console.log(null >= 0);
+// console.log(null < 0);
+
+// console.log(10>=true);
+
+// console.log("Apple" > "Banana");
+
+//different type of conversion will lead to choose common type mostly number then they be compared
+
+// console.log(null >= "");
+
+// console.log(NaN === NaN);
+
+// loops in js
+
+// for(let i = 0; i<=5; i++){
+//     console.log(i);
+// }
+
+//while loop
+
+// let i =3;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+//do while loop
+
+// let i =4;
+// do{
+//     console.log('we are using do while loop to print number here');
+//     console.log(i);
+//     i++;
+// }while(i<12);
