13bcda34ca121e59b061cdeae22fab055195d2df
White-Town <nareshbc5555@gmail.com>
2025-12-15 09:43:20 +0545

    learning typescript

---
+++ typescript/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello, ${person}, Welcome to the Billionare Club`;
+}
+console.log(greet("Kitt"));
+const username = "Kingsman";
+console.log(username);
+//# sourceMappingURL=index.js.map
---
+++ typescript/src/index.ts
@@ -0,0 +1,9 @@
+function greet(person: string): string{
+    return `Hello, ${person}, Welcome to the Billionare Club`;
+}
+
+console.log(greet("Kitt"));
+
+const username: string = "Kingsman";
+
+console.log(username);
