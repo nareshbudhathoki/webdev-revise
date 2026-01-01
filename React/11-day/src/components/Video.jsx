021316e2b8a146e9c07e22c4aa46bad487c1e1c1
White-Town <nareshbc5555@gmail.com>
2026-01-01 12:23:16 +0000

    Completed the video control using the custom button and function, play, resume, stop, backward,forward, reset

--- React/11-day/src/components/Video.jsx
+++ React/11-day/src/components/Video.jsx
@@ -1,12 +1,37 @@
-
+import { useRef } from "react";
 function Video(){
+
+    const videoRef = useRef(null);
+
+    function handleStart(){
+        videoRef.current.play();
+    }
+
+    function handleStop(){
+        videoRef.current.pause();
+    }
+
+    function handlRestart(){
+        videoRef.current.currentTime = 0;
+    }
+
+    function handleForward(){
+        videoRef.current.currentTime +=10;
+    }
+
+    function handleBacward(){
+        videoRef.current.currentTime -= 10;
+    }
+
     return(
         <>
-        <video src="video.mp4" width={600} height={400} controls></video>
+        <video ref={videoRef} src="video.mp4" width={600} height={400} controls></video>
         <div>
-            <button>start</button>
-            <button>pause</button>
-            <button>restart</button>
+            <button onClick={handleStart}>start</button>
+            <button onClick={handleStop}>pause</button>
+            <button onClick={handlRestart}>restart</button>
+            <button onClick={handleBacward}>Backward</button>
+            <button onClick={handleForward}>Forward</button>
         </div>
         </>
     )
